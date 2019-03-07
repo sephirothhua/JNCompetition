a3f64b3c96812e619694ef8012191335fbb4c0c2
Zhaojie_hua <641084377@qq.com>
2019-03-07 11:53:47 +0800

    Update the coco2txt file

--- yolo3/coco2txt.py
+++ yolo3/coco2txt.py
@@ -1,6 +1,7 @@
 import json
 from pycocotools.coco import COCO
 import os
+import re
 def load_coco(dataset_dir, image_dir, class_ids=None,):
     coco = COCO("{}".format(dataset_dir))
     coco_list = []
@@ -50,9 +51,22 @@ def coco2txt(CocoList,TxtDir):
     file.close()
 
 
-JSON_DIR = "./data/json/train_r1.json"
-PIC_DIR = "./data/train_r1"
-TxtDir = "./data/train_r1.txt"
-coco_all = load_coco(JSON_DIR,PIC_DIR)
-coco2txt(coco_all,TxtDir)
+# JSON_DIR = "./data/json/train_r1.json"
+# PIC_DIR = "./data/train_r1"
+# TxtDir = "./data/train_r1.txt"
+# coco_all = load_coco(JSON_DIR,PIC_DIR)
+# coco2txt(coco_all,TxtDir)
 
+
+data_dir = "./data"
+TxtDir = "./data/train_r1.txt"
+coco_list = []
+for root,dirs,files in os.walk(data_dir):
+    for dir in dirs:
+        if(dir.split('_')[0]=="train"):
+            json_file = os.path.join(root,dir,"train_no_poly.json")
+            pic_dir = os.path.join(root,dir,"restricted")
+            coco1 = load_coco(json_file,pic_dir)
+            coco_list.extend(coco1)
+
+coco2txt(coco_list,TxtDir)
