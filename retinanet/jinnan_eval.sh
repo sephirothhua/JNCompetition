d6e30dc3ebbe628ed7b3189bd13776cf9413bbe5
shenhuixiang <shhuixi@qq.com>
2019-03-11 16:13:01 +0800

    添加 sh文件

---
+++ retinanet/jinnan_eval.sh
@@ -0,0 +1,4 @@
+python jinnan_eval.py --threshold 0.05\
+                      --dataset_path /home/shen/Documents/TianChi/classification/data/First_round_data/jinnan2_round1_train_20190305\
+                      --model_path logs/model_final.pt\
+                      --depth 50
